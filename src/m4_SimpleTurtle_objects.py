3b4004be9ee2c756ae84f0f0cce4db6ff9485f09
lauterja <lauterja@rose-hulma.edu>
2018-11-26 17:09:22 -0500

    Done

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Jacob Lauteri.
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving,
 #      ** click anywhere in the window to close the window **.
@@ -73,7 +73,11 @@ import rosegraphics as rg
 #   change this _TODO_ to DONE and  ** continue to the next _TODO_ (below). **
 #
 ###############################################################################
-
+rg.SimpleTurtle()
+#natasha = rg.SimpleTurtle()
+#natasha.forward(100)
+#natasha.speed = 10
+#boris.speed = natasha.speed
 # -----------------------------------------------------------------------------
 # The next few lines show how to:
 #   - CONSTRUCT (make and initialize) a   TurtleWindow   object for animation.
@@ -104,19 +108,53 @@ boris.forward(200)
 #     set its  pen  and  speed  INSTANCE VARIABLES, and ask it to do things.
 # -----------------------------------------------------------------------------
 natasha = rg.SimpleTurtle('turtle')
-natasha.pen = rg.Pen('red', 30)  # Second argument is the Pen's thickness
-natasha.speed = 5  # Bigger means faster, max is usually about 10
+natasha.pen = rg.Pen('orange', 25)  # Second argument is the Pen's thickness
+natasha.speed = 10  # Bigger means faster, max is usually about 10
+boris = rg.SimpleTurtle('turtle')
+boris.pen = rg.Pen('green', 25)
+boris.speed = 7
+sam = rg.SimpleTurtle('turtle')
+sam.pen = rg.Pen('silver', 25)
+sam.speed = 4
 
-natasha.backward(50)
-natasha.right(90)
-natasha.forward(125)
+natasha.backward(25)
+boris.forward(25)
+sam.forward(50)
+natasha.right(125)
+boris.left(125)
+sam.right(25)
+natasha.forward(200)
+boris.backward(200)
+sam.left(400)
+natasha.left(100)
+boris.right(100)
+sam.backward(200)
+natasha.backward(60)
+boris.forward(60)
+sam.right(50)
+natasha.forward(90)
+boris.backward(90)
+sam.forward(100)
+natasha.right(100)
+boris.left(100)
+sam.left(25)
+natasha.left(200)
+boris.right(200)
+sam.backward(50)
+natasha.forward(100)
+boris.backward(100)
+sam.right(200)
+
 
 natasha.speed = 1  # Now slower
-natasha.go_to(rg.Point(-100, 200))
-
+natasha.go_to(rg.Point(-300, 300))
+boris.speed = 10
+boris.go_to(rg.Point(300,300))
+sam.speed = 7
+sam.go_to(rg.Point(0,0))
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more lines of your own code to make one of the existing
 #   SimpleTurtles move some more and/or have different characteristics.
 #
@@ -129,7 +167,7 @@ natasha.go_to(rg.Point(-100, 200))
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       boris    natasha
@@ -156,7 +194,7 @@ natasha.go_to(rg.Point(-100, 200))
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
